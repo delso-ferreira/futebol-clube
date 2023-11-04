c5d155dc5da3f833aab1b266de433304fd6c54b2
delso-ferreira <delsofelipe@gmail.com>
2023-11-04 12:52:10 -0300

    Fix User Migration

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,48 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model<InferAttributes<UserModel>,
+InferCreationAttributes<UserModel>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    timestamps: false,
+    tableName: 'users',
+  },
+);
